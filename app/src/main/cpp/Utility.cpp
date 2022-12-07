a3f2a6611dc44ee0803ecc1353ace65dd7d31bde
wookinlab <19099830@qq.com>
2022-12-07 20:58:24 +0800

    fix build warning.

--- app/src/main/cpp/Utility.cpp
+++ app/src/main/cpp/Utility.cpp
@@ -1,4 +1,4 @@
-#include <sys/time.h>
+#include <ctime>
 
 #include <iostream>
 #include <string>
@@ -12,15 +12,10 @@
 //#endif
 
 uint64_t GetMilliseconds() {
-    struct timeval tv;
+    struct timeval tv = {0};
     gettimeofday(&tv, nullptr);
     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
-
-std::string gHelloWorldC =  "kernel void helloworld(global const int* in, global int* out) {\n"
-                            "  int num = get_global_id(0);"
-                            "  out[num] = in[num] * 2.0;"
-                            "}";
 
 // CL_DEVICE_TYPE_ALL
 // CL_DEVICE_TYPE_CPU
@@ -28,27 +23,32 @@ std::string gHelloWorldC =  "kernel void helloworld(global const int* in, global
 void GetDevices(const std::vector<cl::Platform>& platforms,
         cl_device_type type,
         std::vector<cl::Device>* devices) {
-    for(int i = 0; i < platforms.size(); ++i) {
+    for(const cl::Platform& pl: platforms) {
         std::vector<cl::Device> temp_devices;
-        cl_int err = platforms[i].getDevices(type, &temp_devices);
+        cl_int err = pl.getDevices(type, &temp_devices);
         if (err != CL_SUCCESS) {
-            LOGI("Can not get any devices of type: %d, error code: %d", type, err);
+            LOGI("Can not get any devices of type: %d, error code: %d", (int32_t)type, err);
         }
         devices->insert(devices->end(), temp_devices.begin(), temp_devices.end());
     }
 }
 
 void TryAdd() {
+    std::string HelloWorldC =  "kernel void helloworld(global const int* in, global int* out) {\n"
+                                "  int num = get_global_id(0);"
+                                "  out[num] = in[num] * 2.0;"
+                                "}";
+
     // get platforms
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    if (platforms.size() == 0) { LOGI("can not get any opencl platforms."); return; }
+    if (platforms.empty()) { LOGI("can not get any opencl platforms."); return; }
     // std::string platform_name = platforms[0].getInfo<CL_PLATFORM_NAME>();
 
     // get devices of GPU
     std::vector<cl::Device> devices;
     GetDevices(platforms, CL_DEVICE_TYPE_GPU, &devices);
-    if (devices.size() == 0) { LOGI("can not get any GPU devices."); return; }
+    if (devices.empty()) { LOGI("can not get any GPU devices."); return; }
     cl::Device& target_device = devices[0];
     std::string use_device_name = target_device.getInfo<CL_DEVICE_NAME>();
 
@@ -58,7 +58,7 @@ void TryAdd() {
 
     // build device program
     cl::Program::Sources sources;
-    sources.push_back({gHelloWorldC.c_str(), gHelloWorldC.length()});
+    sources.push_back({HelloWorldC.c_str(), HelloWorldC.length()});
     cl::Program program_ = cl::Program(context, sources);
     if ((err = program_.build({target_device})) != CL_SUCCESS) {
         std::string s = "Error building: " + program_.getBuildInfo<CL_PROGRAM_BUILD_LOG>(target_device);
@@ -75,7 +75,7 @@ void TryAdd() {
     int* output = new int[NUM];
 
     cl::Buffer inputBuffer = cl::Buffer(context, CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR, (NUM) * sizeof(int), (void*)input, &err);
-    cl::Buffer outputBuffer = cl::Buffer(context, CL_MEM_WRITE_ONLY, NUM * sizeof(int), NULL, &err);
+    cl::Buffer outputBuffer = cl::Buffer(context, CL_MEM_WRITE_ONLY, NUM * sizeof(int), nullptr, &err);
 
     // create queue
     cl::CommandQueue queue(context, devices[0], 0, &err);
@@ -91,13 +91,13 @@ void TryAdd() {
     /**Step 10: Running the kernel.*/
     // run kernel function
     cl::Event event;
-    err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(NUM), cl::NullRange, NULL, &event);
+    err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(NUM), cl::NullRange, nullptr, &event);
     // event.wait();
     queue.finish();
 
     /**Step 11: Read the cout put back to host memory.*/
     // read result, read the result put back to host memory
-    queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, NUM * sizeof(int), output, 0, NULL);
+    queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, NUM * sizeof(int), output, nullptr, nullptr);
     std::cout << output[NUM-1] << std::endl;
 
     /**Step 12: Clean the resources.*/
@@ -113,17 +113,17 @@ std::string GetDevicesName() {
 
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    if (platforms.size() == 0) {
+    if (platforms.empty()) {
         return "no platforms";
     }
-    for (int i = 0; i < platforms.size(); ++i) {
+    for (const cl::Platform& pl: platforms) {
         std::vector<cl::Device> devices;
-        platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices);
-        for (int j = 0; j < devices.size(); ++j) {
+        pl.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+        for (const cl::Device& device : devices) {
             sz += "name ";
-            sz += std::string(devices[j].getInfo<CL_DEVICE_NAME>()); // device name
+            sz += std::string(device.getInfo<CL_DEVICE_NAME>()); // device name
             sz += ", vendor ";
-            sz += std::string(devices[j].getInfo<CL_DEVICE_VENDOR>());
+            sz += std::string(device.getInfo<CL_DEVICE_VENDOR>());
             sz += " | ";
         }
     }
